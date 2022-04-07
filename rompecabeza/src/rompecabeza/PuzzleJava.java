86e89c89ee709aea6f11644b0a6171da874e3cee
13232-art <pdurannu@everis.com>
2022-04-07 11:21:46 -0300

    Correccion

--- rompecabeza/src/rompecabeza/PuzzleJava.java
+++ rompecabeza/src/rompecabeza/PuzzleJava.java
@@ -1,35 +1,52 @@
 package rompecabeza;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
 public class PuzzleJava {
 
 	public static void main(String[] args) {
-
-		ArrayList<Integer> numeros = new ArrayList<Integer>();
-
-		numeros.add(3);
-
-		numeros.add(5);
-
-		numeros.add(1);
-
-		numeros.add(2);
-
-		numeros.add(7);
 		
-		numeros.add(9);
-		
-		numeros.add(8);
-		
-		numeros.add(13);
-		
-		numeros.add(25);
-		
-		numeros.add(32);
-		
+	  //Primera Actividad
+
+//		ArrayList<Integer> numeros = new ArrayList<Integer>();
+//
+//		numeros.add(3);
+//
+//		numeros.add(5);
+//
+//		numeros.add(1);
+//
+//		numeros.add(2);
+//
+//		numeros.add(7);
+//		
+//		numeros.add(9);
+//		
+//		numeros.add(8);
+//		
+//		numeros.add(13);
+//		
+//		numeros.add(25);
+//		
+//		numeros.add(32);
+//		
+//		Collections.shuffle(numeros);
+//
+//		System.out.println(numeros); 
+//
+//		Collections.sort(numeros);
+//
+//		System.out.println(numeros); 
+//
+//		Random r = new Random();
+//
+//		System.out.println(r.nextInt()); 
+//
+//		System.out.println(r.nextInt(5)); 
+//		
 //		int a[] = new int[] { 3, 5, 2,7,9,8,13,25,32 };
 //		int sum = 0;
 //		for (int i = 0; i < a.length; i++) {
@@ -43,7 +60,7 @@ public class PuzzleJava {
 //			}
 //		}
 		
-		
+	   //Segunda Actividad
 //		ArrayList<String> nombres = new ArrayList<String>();
 //		
 //        nombres.add("Nancy");
@@ -63,7 +80,7 @@ public class PuzzleJava {
 //			}
 //		}
 		
-		
+	  //Tercera Actividad
 //		ArrayList<Character> letras = new ArrayList<Character>();
 //		
 //		letras.add('a');
@@ -102,36 +119,24 @@ public class PuzzleJava {
 //		char ultima = le[le.length-1];
 //		System.out.println(ultima);
 
+	   //Cuarta Actividad
 		
-		int[] Arregl = new int[100];
-		Random random = new Random();
-		int y = 55;
-		if(Arregl.length >= y) {
-		System.out.println(random.nextInt(100));
-		}
-        
-			//System.out.println(y);
-		//for (int y = 55; y <= Arregl.length; y++) {
-			//if(y > random.nextInt(56)) {
-		   // System.out.println(Arregl[y]);
-		//}
+//		int[] Arregl = new int[10];
+//		Random random = new Random();
+//		int low = 55;
+//		int high = 100;
+//		
+//		for (int y = 0; y < Arregl.length; y++) {
+//			int result = random.nextInt(high-low) + low;
+//			System.out.println(result);
+//			Arregl[y] = result;//asignando a la posicion el resultado
+//			}
+//		System.out.println(Arrays.toString(Arregl));
+
+		
 
 
-       
-	
-		Collections.shuffle(numeros);
 
-		System.out.println(numeros); // [5, 4, 3, 2, 1]
-
-		Collections.sort(numeros);
-
-		System.out.println(numeros); // [1, 2, 3, 4, 5]
-
-		Random r = new Random();
-
-		System.out.println(r.nextInt()); // Sin límites
-
-		System.out.println(r.nextInt(5)); // Con límites entre 0 y 5
 
 	}
 

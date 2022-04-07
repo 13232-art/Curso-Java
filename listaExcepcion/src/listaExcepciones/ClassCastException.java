b4351c2af901568ee217caf45d7ef966e750b896
13232-art <pdurannu@everis.com>
2022-04-07 10:20:31 -0300

    Octava Actividad

--- listaExcepcion/src/listaExcepciones/ClassCastException.java
+++ listaExcepcion/src/listaExcepciones/ClassCastException.java
@@ -1,8 +1,29 @@
 package listaExcepciones;
 
+import java.util.ArrayList;
+
 public class ClassCastException {
 
 	public static void main(String[] args) {
+
+		ArrayList<Object> myList = new ArrayList<Object>();
+		try {
+			myList.add("13");
+
+			myList.add("Hola Mundo");
+
+			myList.add(48);
+
+			myList.add("Adios Mundo");
+		} catch (Exception e) {
+			for (int i = 0; i < myList.size(); i++) {
+
+				int castedValue = (int) myList.get(i);
+				System.out.println(castedValue);
+				System.out.println(e);
+			}
+
+		}
 		
 
 	}
